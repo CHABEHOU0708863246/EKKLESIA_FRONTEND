11cac60f9244866290141f1f343306c6d80ced2a
AFFERY AUXENCE DELORS CHABEHOU <afferyauxencedelorschabehou@gmail.com>
2026-07-10 18:14:24 +0000

    Conteneurisation de FRONTEND 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // ✅ URL du BACKEND en production (API déployée)
-  apiUrl: 'https://ekklesia-backend.onrender.com',
+  apiUrl: 'https://ekklesia-backend-n6zs.onrender.com',
 
 };
